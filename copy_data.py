1233c827fcf1db79f1b5ee5cdaa42463f54451d2
Anshul Tomar <decoder746@github.com>
2021-02-10 07:29:39 +0530

    copy

--- copy_data.py
+++ copy_data.py
@@ -5,16 +5,18 @@ domains = ["soccer","birthday","wedding","friends","surveillance","techtalk"]
 root_dir = "/media/hdd1/home/vishal/data/"
 budgets = ["60","90","120","150","180"]
 algo = ["vasnet-last","vasnet-max","vslstm-max","vslstm-last"]
-algo = ["mixture-cfg"]
+algo = ["mixture-cfg6"]
 for domain in domains:
     for budget in budgets:
         for al in algo:
             path = os.path.join(root_dir,domain,budget,al)
             videos = os.listdir(path)
             for video in videos:
-                video_name = os.listdir(os.path.join(root_dir,domain,budget,al,video))[0]
-                video_path = os.path.join(root_dir,domain,budget,al,video,video_name)
-                new_summary_video = video+"_dr-dsn_"+budget+".json"
+                # video_name = os.listdir(os.path.join(root_dir,domain,budget,al,video))[0]
+                video_name =video.split(al)[0][:-1]
+                # video_path = os.path.join(root_dir,domain,budget,al,video,video_name)
+                video_path = os.path.join(root_dir,domain,budget,al,video)
+                new_summary_video = video_name+"_dr-dsn_"+budget+".json"
                 cp_dir = os.path.join(rdir,"submission"+al,domain,budget,"dr-dsn")
                 cp_path = os.path.join(rdir,"submission"+al,domain,budget,"dr-dsn",new_summary_video)
                 cmd = "mkdir -p {}".format(cp_dir)
